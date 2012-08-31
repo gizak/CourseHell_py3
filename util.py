f043cb296b175ee092a7360721047d6800d95ef9
Gizak <zaknafein.dourden.2012@gmail.com>
2012-08-31 12:51:09 +0800

    first ed

--- util.py
+++ util.py
@@ -1,10 +1,13 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+
 import urllib.request
 import urllib.parse
 import urllib.response
 import re
 import webbrowser
 import time
-import os
+import sys
 
 class Capture:
 	__url_login='http://jwas3.nju.edu.cn:8080/jiaowu/login.do'
@@ -26,7 +29,9 @@ class Capture:
 	def cookie_init(self):
 		opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor())
 		urllib.request.install_opener(opener)
-		conn=urllib.request.urlopen(self.__url_login,urllib.parse.urlencode(self.__token_login).encode('utf-8'))
+		urllib.request.urlopen(self.__url_login,urllib.parse.urlencode(self.__token_login).encode('utf-8'))
+		self.__get_raw()
+		self.__fetch_array()
 
 	def __get_raw(self):
 		self.__data_raw=urllib.request.urlopen(self.__url_catch).read()
@@ -34,35 +39,40 @@ class Capture:
 	def __get_major(self):
 		pass
 
-	def __get_tr_batch(self):
-		return re.findall(r'(?<=<tr class="TABLE_TR_0[12]">).*?(?=</tr>)',self.__data_raw)
-
-	def __get_row(self,tr):
-		return re.findall(r'(?<=<td valign="middle">)[^<]+(?=</td>)|(?<=<td align="center" valign="middle">)\w+|(?<=<td>).*?(?=</td></tr>)',tr)
-
 	def __fetch_array(self):
-		rs=self.__get_tr_batch()
+		rs=re.findall(r'(?<=<tr class="TABLE_TR_0[12]">).*?(?=</tr>)',self.__data_raw.decode())
 		for item in rs:
-			self.__store.append(self.__get_row(item))
+			self.__store.append(re.findall(r'(?<=<td valign="middle">)[^<]+(?=</td>)|(?<=<td align="center" valign="middle">)\w+|(?<=<td>).*?(?=</td></tr>)',item))
 
 	def get_store(self,):
-		if not self.__store :
-			self.get_raw()
-			self.__fetch_array()
 		return self.__store
 
 	def refresh(self):
-		pass
+		self.__get_raw()
+		self.__fetch_array()
 
 	def dump(self):
-		self.__get_raw()
-		print(self.__data_raw)
+		for it in self.__store:
+			print(it,'\n')
+
+	def detect(self):
+		rs=[]
+		flag_t=0
+		for it in self.__store:
+			if int(it[5]) != 0 : flag_t=-1
+			if int(it[6+flag_t]) > int(it[7+flag_t]) :
+				rs.append(int(it[0]))
+			flag_t=0
+		return rs
 
 
 if __name__ == '__main__':
-	#ins=Capture()
+	ins=Capture()
+	cnt=0
+	while len(ins.detect())==0:
+		time.sleep(1)
+		ins.refresh()
+		print('Round[',cnt,'] None')
+		cnt+=1
+	print('Detected leak ! ',ins.detect())
 	#ins.dump()
-	b=b'\r\n\r\n\r\n<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\r\n<html xmlns="http://www.w3.org/1999/xhtml">\r\n  <head>\r\n    <title>\xe5\x8d\x97\xe4\xba\xac\xe5\xa4\xa7\xe5\xad\xa6\xe6\x95\x99\xe5\x8a\xa1\xe7\xb3\xbb\xe7\xbb\x9f</title>'
-	print(type(b.decode('utf-8')))
-	os.system('pause')
-	
