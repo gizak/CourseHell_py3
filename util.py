291f0daf09aa268169f156694148d41ec8db4067
Gizak <zaknafein.dourden.2012@gmail.com>
2012-09-02 19:41:31 +0800

    done

--- util.py
+++ util.py
@@ -14,7 +14,11 @@ class Capture:
 	__url_campus_gl='%e9%bc%93%e6%a5%bc%e6%a0%a1%e5%8c%ba'
 	__url_campus_xl='%E4%BB%99%E6%9E%97%E6%A0%A1%E5%8C%BA' 
 	__url_campus=None
+<<<<<<< HEAD
 	__url_catch='http://jwas3.nju.edu.cn:8080/jiaowu/student/elective/courseList.do?method=discussRenewCourseList&campus='
+=======
+	__url_catch='http://jwas3.nju.edu.cn:8080/jiaowu/student/elective/courseList.do?method=publicRenewCourseList&campus='
+>>>>>>> done
 	__url_submit='http://jwas3.nju.edu.cn:8080/jiaowu/student/elective/courseList.do?method=submitDiscussRenew&campus='
 	__store=[]
 	__token_login={'userName':'091279049','password':'000000'} 
@@ -22,11 +26,15 @@ class Capture:
 	__data=''
 
 	def __init__(self):
+<<<<<<< HEAD
 		self.__url_campus=self.__url_campus_xl
 		self.__url_catch+=self.__url_campus
 		self.__url_submit+=self.__url_campus
+=======
+>>>>>>> done
 		self.token_assign()
 		self.cookie_init()
+		self.campus_assign()
 		self.__get_raw()
 		self.__fetch_array()		
 
@@ -35,16 +43,35 @@ class Capture:
 		self.__token_login['password']=input('Password:')
 		print('\n')
 
+	def campus_assign(self):
+		choose=int(input('choose campus: 0:GuLou, 1:XianLin  '))
+		if choose == 0 :
+			self.__url_campus=self.__url_campus_gl
+		else:
+			self.__url_campus=self.__url_campus_xl
+		self.__url_catch+=self.__url_campus
+		self.__url_submit+=self.__url_campus
+		
 	def cookie_init(self):
 		opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor())
 		urllib.request.install_opener(opener)
 		login=urllib.request.urlopen(self.__url_login,urllib.parse.urlencode(self.__token_login).encode('utf-8'))
+<<<<<<< HEAD
 		'''
 		if login.geturl()==self.__url_login :
 			print('login failed, retry:\n')
 			self.token_assign()
 			self.cookie_init()
 		'''
+=======
+		is_valid=re.search(r'<div id="UserInfo">',login.read().decode())
+		if not is_valid :
+			print('login failed, retry:\n')
+			self.token_assign()
+			self.cookie_init()
+		else:
+			print('start:\n')
+>>>>>>> done
 		    
 	def __get_raw(self):
 		self.__data_raw=urllib.request.urlopen(self.__url_catch).read()
@@ -73,27 +100,35 @@ class Capture:
 			print(it,'\n')
 
 	def detect(self):
-		rs=[]
+		rs_cid=[]
+		rs_idx=[]
+		idx_cnt=0
 		flag_t=0 #is non-teacher ? assume have t
 		for it in self.__store:
 			if int(it[5]) != 0 : flag_t=-1 #no t
 			if int(it[6+flag_t]) > int(it[7+flag_t]) :
-				rs.append(it[-1])
+				rs_cid.append(it[-1])
+				rs_idx.append(idx_cnt)
 			flag_t=0
-		return rs
+			idx_cnt+=1
+		return (rs_cid,rs_idx)
 
 
 if __name__ == '__main__':
 	ins=Capture()
 	cnt=0
-	while len(ins.detect())==0:
+	info_detect=ins.detect()
+	while len(info_detect[0])==0:
 		time.sleep(1)
 		ins.refresh()
+		info_detect=ins.detect()
 		print('Round[',cnt,'] None')
 		cnt+=1
-	class_id_set=ins.detect()
+	class_id_set=info_detect[0]
 	print('Detected leak !')
-	print('class_id=',class_id_set[0],'\nonSubmit...')
+	print('classId=',class_id_set[0])
+	#print('dump:',ins.get_store()[info_detect[1][0]])
+	print('onSubmit...')
 	ins.submit(class_id_set[0])
 	print('submit complete!')
 	#ins.dump()
